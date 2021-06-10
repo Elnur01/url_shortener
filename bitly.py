700c3371c43bb5f24deb0281a3584c1686142723
Elnur Huseynov <30310749+Elnur01@users.noreply.github.com>
2021-06-10 18:23:44 +0400

    commit

--- bitly.py
+++ bitly.py
@@ -4,14 +4,14 @@ import bitly_api
 #define bitly token
 BITLY_ACCESS_TOKEN = "29929fc8cb327c419d60ac7764d9521f63562042"
 
-#Get url from user/client
-URL = input("URL that you want to shorten: ")
-
 #Connect token with function
-t = bitly_api.Connection(access_token=BITLY_ACCESS_TOKEN)
+b = bitly_api.Connection(access_token=BITLY_ACCESS_TOKEN)
+
+#Get url from user/client
+URL = str(input("URL that you want to shorten: "))
 
 #Shorten the link
-response = t.shorten(URL)
+response = b.shorten('http://google.com/')
 
 #printing out short link :)
 print(response)
